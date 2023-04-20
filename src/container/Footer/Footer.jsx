6d1533f2bec0eb04519d43ee8b3203bc72abe51f
pawan <bpawan277@gmail.com>
2023-04-20 20:07:56 +0530

    pawan

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import arrow from '../../assets/arrowdown.png';
-import './Footer.css';
+import React from "react";
+import arrow from "../../assets/arrowdown.png";
+import github from "../../assets/githubimg.png";
+import linkdin from "../../assets/linkdin.png";
 
+import "./Footer.css";
 function Footer() {
+  function setgithub() {
+    window.location.href = "https://github.com/277pawan";
+  }
+  function setlinkedin() {
+    window.location.href = "https://www.linkedin.com/in/pawan-bisht-a943161b9/";
+  }
   return (
-    <div className='footercontainer'>
-      <div className='footerheadlight_view'>Do you want to step into the <br></br>future before others.<br></br>
-      Join Us.<br></br>
-      <img className="arrowcss"src={arrow} alt="arrowdown"></img></div>
-   </div>
-  )
+    <div className="footercontainer">
+      <div className="footerheadlight_view">
+        Do you want to step into the <br></br>future before others.<br></br>
+        Join Us.<br></br>
+        <img className="arrowcss" src={arrow} alt="arrowdown"></img>
+        <br></br>
+        <span>
+          <img
+            className="github"
+            src={github}
+            alt="arrowdown"
+            onClick={setgithub}
+          ></img>
+          &nbsp;
+          <img
+            className="linkdin"
+            src={linkdin}
+            alt="arrowdown"
+            onClick={setlinkedin}
+          ></img>
+        </span>
+        <br></br>
+        <p className="copyright">
+          <abbr title="Pawan Bisht">&copy; Educational purpose</abbr>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
